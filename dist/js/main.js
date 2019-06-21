9c6456cee27747dc0fc0906dd147943132d6bde6
Jeannie Chen <jingpiano@ucla.edu>
2019-06-21 00:35:31 -0700

    tinkering with image slideshow, settled on temporary template with simple animations and css, look at more tutorials later, complex slideshow not needed at the moment

--- dist/js/main.js
+++ dist/js/main.js
@@ -74,15 +74,14 @@ function topFunction() {
 }
 
 // Slideshow
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
